59fad8fe2e9fda5567b8cbe0177ffe21f3b3bfc7
robincajas12 <robincajas2020@gmail.com>
2025-10-15 16:22:35 -0500

    a

--- _kanjiHover.js
+++ _kanjiHover.js
@@ -1,35 +1,36 @@
-var body = document.getElementById('kanjiHover')
-if (body) body = body.innerHTML
-var kanji = new Set()
-var kanjiDict = {}
+var body = document.getElementById('kanjiHover');
+if (body) body = body.innerHTML;
+
+var kanji = new Set();
+var kanjiDict = {};
 
 if (body) {
-  appendCSS()
-  appendHoverDiv()
-  findKanji()
-  getKanjiData().then(r => {
-    let kanjiTargets = document.getElementsByClassName("kanjiHoverTarget")
+  appendCSS();
+  appendHoverDiv();
+  findKanji();
+  getKanjiData().then(() => {
+    let kanjiTargets = document.getElementsByClassName("kanjiHoverTarget");
     Array.from(kanjiTargets).forEach(function (element) {
-      element.addEventListener('mouseenter', onKanjiHover)
-      element.addEventListener('mouseleave', onKanjiUnhover)
+      element.addEventListener('mouseenter', onKanjiHover);
+      element.addEventListener('mouseleave', onKanjiUnhover);
     });
-  })
+  });
 }
 
 function appendHoverDiv() {
-  var hoverDiv = document.createElement('div')
-  hoverDiv.classList.add("hoverDiv")
-  document.body.appendChild(hoverDiv)
+  var hoverDiv = document.createElement('div');
+  hoverDiv.classList.add("hoverDiv");
+  document.body.appendChild(hoverDiv);
 }
 
 function findKanji() {
-  const regex = /([\u4E00-\u9FAF])(?![^<]*>|[^<>]*<\/g)/g
-  const matches = body.matchAll(regex)
+  const regex = /([\u4E00-\u9FAF])(?![^<]*>)/g;
+  const matches = body.matchAll(regex);
   for (const match of matches) {
-    kanji.add(...match)
+    kanji.add(match[1]);
   }
 
-  body = body.replace(regex, (match) => `<span class="kanjiHoverTarget">${match}</span>`)
+  body = body.replace(regex, (match) => `<span class="kanjiHoverTarget">${match}</span>`);
   document.getElementById('kanjiHover').innerHTML = body;
 }
 
@@ -51,92 +52,82 @@ async function getKanjiData() {
           res = await fetch(fallbackUrl);
         }
 
-        // If the second fetch also fails, this will throw and be caught.
         if (!res.ok) {
-            throw new Error(`Kanji '${character}' not found in either source.`);
+          throw new Error(`Kanji '${character}' not found in either source.`);
         }
-        
-        return res.json();
+
+        const data = await res.json();
+        return data;
 
       } catch (error) {
         console.error(error);
-        // Return a null or an error object so Promise.all doesn't fail completely
-        return { kanji: character, error: "Not found" }; 
+        return { kanji: character, error: "Not found" };
       }
     })
   );
 
-  // Populate dictionary with kanji as key, filtering out any that failed
+  // Populate dictionary with kanji as key
   for (let item of kanjiArr) {
-    if (item && !item.error) {
-        kanjiDict[item["kanji"]] = buildString(item);
+    if (item && item.kanji && !item.error) {
+      kanjiDict[item.kanji] = buildString(item);
     }
   }
 }
 
 function onKanjiHover(event) {
-  let hoverDiv = document.getElementsByClassName("hoverDiv")[0]
-  hoverDiv.innerHTML = kanjiDict[event.target.innerHTML]
-  hoverDiv.style.display = "block"
+  let hoverDiv = document.getElementsByClassName("hoverDiv")[0];
+  let content = kanjiDict[event.target.innerHTML];
+  if (content) {
+    hoverDiv.innerHTML = content;
+    hoverDiv.style.display = "block";
+  }
 }
 
 function onKanjiUnhover(event) {
-  let hoverDiv = document.getElementsByClassName("hoverDiv")[0]
-  hoverDiv.innerHtml = ""
-  hoverDiv.style.display = "none"
+  let hoverDiv = document.getElementsByClassName("hoverDiv")[0];
+  hoverDiv.innerHTML = "";
+  hoverDiv.style.display = "none";
 }
 
 function injectKanjiHTML() {
-  var str = document.getElementById("kanjiHover").innerHTML
-
+  var str = document.getElementById("kanjiHover").innerHTML;
   var re = new RegExp(Object.keys(kanjiDict).join("|"), "gi");
   str = str.replace(re, function (matched) {
     if (kanjiDict[matched])
-      return buildString(matched)
-
-    return matched
+      return buildString(kanjiDict[matched]);
+    return matched;
   });
-
-  document.getElementById("kanjiHover").innerHTML = str
+  document.getElementById("kanjiHover").innerHTML = str;
 }
 
 function buildString(kanjiData) {
-  let s = '<span class="hoverText">Kanji:</span> ' + kanjiData.kanji + '<br>'
+  let s = '<span class="hoverText">Kanji:</span> ' + kanjiData.kanji + '<br>';
+
   if (kanjiData.grade)
-    s += '<span class="hoverText">Grade:</span> ' + kanjiData.grade + '<br>'
-  s += '<span class="hoverText">Meaning:</span> '
-  for (let str of kanjiData.meanings) {
-    s += str + ', '
+    s += '<span class="hoverText">Grade:</span> ' + kanjiData.grade + '<br>';
+
+  s += '<span class="hoverText">Meaning:</span> ';
+  if (kanjiData.meanings && kanjiData.meanings.length > 0)
+    s += kanjiData.meanings.join(', ');
+  else if (kanjiData.heisig_en)
+    s += kanjiData.heisig_en;
+  else
+    s += 'Unknown';
+  s += '<br>';
+
+  if (kanjiData.kun_readings && kanjiData.kun_readings.length > 0) {
+    s += `<span class="hoverText">Kun'yomi:</span> ${kanjiData.kun_readings.join(', ')}<br>`;
   }
 
-  s = s.slice(0, -2)
-  s += '<br>'
-
-  if (kanjiData.kun_readings.length > 0) {
-    s += '<span class="hoverText">Kun\'yomi:</span> '
-    for (let str of kanjiData.kun_readings) {
-      s += str + ', '
-    }
-    s = s.slice(0, -2)
-    s += '<br>'
+  if (kanjiData.on_readings && kanjiData.on_readings.length > 0) {
+    s += `<span class="hoverText">On'yomi:</span> ${kanjiData.on_readings.join(', ')}<br>`;
   }
 
-  if (kanjiData.on_readings.length > 0) {
-    s += '<span class="hoverText">On\'yomi:</span> '
-    for (let str of kanjiData.on_readings) {
-      s += str + ', '
-    }
-    s = s.slice(0, -2)
-    s += '<br>'
-  }
-
-  s += '</span>'
-
-  return s
+  return s;
 }
-// cual es el cuerpo de kanjiData? 
+
 function appendCSS() {
-  var styleSheet = document.createElement('style')
+  var styleSheet = document.createElement('style');
   styleSheet.innerHTML = `
   .hoverDiv {
     position: absolute;
@@ -149,9 +140,8 @@ function appendCSS() {
     z-index: 1;
     display: none;
     position: fixed;
-      top: 50%;
+    top: 50%;
     left: 50%;
-    -webkit-transform: translate(-50%, -50%);
     transform: translate(-50%, -50%);
     font-size: 18px;
     writing-mode: horizontal-tb;
@@ -164,6 +154,6 @@ function appendCSS() {
   @media only screen and (max-width: 768px) {
       .hoverDiv {width: 90vw;}
   } 
-  `
-  document.head.appendChild(styleSheet)
+  `;
+  document.head.appendChild(styleSheet);
 }
